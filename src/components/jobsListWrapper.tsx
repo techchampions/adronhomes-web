4c451cab828bf13f1a773073ff8decd73f30455b
John Dev <kingsoorsa@gmail.com>
2025-08-19 15:18:55 +0100

    ffggfh

---
+++ src/components/jobsListWrapper.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useJobListPage } from "@/data/hooks";
+import Loader from "./Loader";
+import JobsList from "./JobsList";
+import ApiErrorBlock from "./ApiErrorBlock";
+import NoPropertyFound from "@/components/NoPropertyFound";
+import Button from "@/components/Button";
+
+const JobsListWrapper = () => {
+  const { data, isLoading, isError } = useJobListPage(1);
+
+  if (isLoading) return <Loader />;
+  if (isError) return <ApiErrorBlock />;
+
+  const jobs = data?.data.jobs_post.data || [];
+  console.log(jobs.length);
+  if (jobs.length < 1) {
+    return <NoPropertyFound text="No Jobs found." />;
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full px-8">
+      <div className="w-full rounded-[50px] px-8 py-5 mt-10 text-left mb-20">
+        <JobsList jobs={jobs.slice(0, 3)} />
+      </div>
+      <Button
+        label="View Jobs"
+        onClick={() => (window.location.href = "/careers/jobs")}
+        className="bg-adron-green px-4 text-sm !w-fit"
+      />
+    </div>
+  );
+};
+export default JobsListWrapper;
