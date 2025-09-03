6c9a87e57fa770a75b51228377c23a3e36cee050
King Soorsa <kingsoorsa@gmail.com>
2025-09-03 17:04:56 +0100

    Soosar (#11)
    * Create netlify.toml
    
    * Update package.json
    
    * Update package-lock.json
    
    * Update package.json
    
    * Delete netlify.toml
    
    * Update package.json
    
    * Update package-lock.json
    
    * install and setup netlify plugin
    
    * fixed social icons values
    
    * Added dashboard link
    
    * added property enquiry request
    
    * fixed footer,virtual tour
    
    * consumed FAQs endpoint
    
    * apply for jobs
    
    * jobs, filter,application
    
    * feetet
    
    * ffdg
    
    * fdff
    
    * gfgf
    
    * ffggfh
    
    * sfssf
    
    * sfssfdfd
    
    * updated testimonial slider
    
    * fssfsf
    
    * fix:monday fixes
    
    * sfsff
    
    * dshjsf
    
    * dshjsfadss
    
    * ssf
    
    * thurday
    
    * fsf
    
    * fsgug

--- src/data/types/GetPropertyByIdResponse.ts
+++ src/data/types/GetPropertyByIdResponse.ts
@@ -37,6 +37,27 @@ export interface PropertyDetail {
   updated_at: string;
 }
 
+// export interface Property {
+//   id: number;
+//   name: string;
+//   display_image: string;
+//   photos: string[];
+//   size: string;
+//   price: number;
+//   type: PropertyType; // changed from number to object
+//   slug: string;
+//   features: string[];
+//   overview: string;
+//   description: string;
+//   street_address: string;
+//   country: string;
+//   state: string;
+//   lga: string;
+//   details: PropertyDetail[];
+//   nearby_landmarks: string;
+//   created_at: string | null;
+//   updated_at: string | null;
+// }
 export interface Property {
   id: number;
   name: string;
@@ -44,6 +65,7 @@ export interface Property {
   photos: string[];
   size: string;
   price: number;
+  initial_deposit: number | null;
   type: PropertyType; // changed from number to object
   slug: string;
   features: string[];
@@ -53,8 +75,43 @@ export interface Property {
   country: string;
   state: string;
   lga: string;
-  details: PropertyDetail[];
-  nearby_landmarks: string;
   created_at: string | null;
   updated_at: string | null;
+  total_amount: number | null;
+  is_bought: boolean;
+  is_saved: boolean;
+  is_discount: boolean;
+  discount_name: string;
+  discount_percentage: number | string;
+  no_of_bedroom: number;
+  number_of_bathroom: number;
+  year_built: string;
+  parking_space: string | number;
+  area: string;
+  property_map: string | null;
+  property_video: string | null;
+  virtual_tour: string | null;
+  subscriber_form: string;
+  status: string;
+  property_duration_limit: number;
+  payment_schedule: string[];
+  payment_type: string;
+  is_sold: number;
+  is_active: number;
+  details: PropertyDetail[];
+  // saved_property: SavedProperty | null;
+  // bought_property: BoughtProperty | null;
+  number_of_unit: number;
+  unit_available: number;
+  property_agreement: string;
+  whatsapp_link: string;
+  video_link: string;
+  contact_number: string;
+  topography: string;
+  road_access: string;
+  gated_estate: string;
+  fencing: string;
+  purpose: string[] | null;
+  nearby_landmarks: string[] | null;
+  title_document_type: string | null;
 }
